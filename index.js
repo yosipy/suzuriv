d44e03081c3480c011148b5702a32528eb65fbf7
yosipy <todacchi.desu@gmail.com>
2020-08-06 22:23:15 +0900

    test

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-//const res1 = await axios.get('https://suzuri.jp/oauth/authorize?client_id=5h4E1jWnJDWgPBnxwgR_m_R-2UxiRlNl7yARrbhHlMk&scope=read&redirect_uri=https://yosipy.github.io/suzuriv/&response_type=code')
-//console.log(res1)
+const res1 = await axios.get('https://suzuri.jp/oauth/authorize?client_id=5h4E1jWnJDWgPBnxwgR_m_R-2UxiRlNl7yARrbhHlMk&scope=read&redirect_uri=https://yosipy.github.io/suzuriv/&response_type=code')
+console.log(res1)
