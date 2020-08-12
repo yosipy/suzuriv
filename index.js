0515165eb8fa3fd48b3af54e859ecb2721ada2ca
yosipy <todacchi.desu@gmail.com>
2020-08-12 16:18:52 +0900

    add

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+xhr = new XMLHttpRequest();
+
 async function oauth() {
-    const res1 = await axios.get('https://suzuri.jp/oauth/authorize?client_id=5h4E1jWnJDWgPBnxwgR_m_R-2UxiRlNl7yARrbhHlMk&scope=read&redirect_uri=https://yosipy.github.io/suzuriv/&response_type=code')
-    console.log(res1)
+  const res = await axios.get('https://suzuri.jp/oauth/authorize', {
+		client_id: '5h4E1jWnJDWgPBnxwgR_m_R-2UxiRlNl7yARrbhHlMk', 
+		scope: 'read', 
+		redirect_uri: 'https://yosipy.github.io/suzuriv/', 
+		response_type: 'code'
+	})
+	console.log(res)
+	res = await axios.post('https://suzuri.jp/oauth/token', {
+		grant_type: 'authorization_code',
+		code: ''
+	})
 }
 oauth()
