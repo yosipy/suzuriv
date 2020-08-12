ba7ecd0a4b5165904c7fa871c775b2a6916d6cca
yosipy <todacchi.desu@gmail.com>
2020-08-12 16:24:06 +0900

    add

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function oauth() {
-  const res = await axios.get('https://suzuri.jp/oauth/authorize', {
+async function oauth() {
+  let res = await axios.get('https://suzuri.jp/oauth/authorize', {
 		client_id: '5h4E1jWnJDWgPBnxwgR_m_R-2UxiRlNl7yARrbhHlMk', 
 		scope: 'read', 
 		redirect_uri: 'https://yosipy.github.io/suzuriv/', 
