4e0e94ae4ed6dbfd7470937d8c9244aaee74245b
yosipy <todacchi.desu@gmail.com>
2020-08-13 18:34:49 +0900

    add

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function oauth() {
-  let res = await axios.get('https://suzuri.jp/oauth/authorize', {
+  /*let res = await axios.get('https://suzuri.jp/oauth/authorize', {
 		params: {
 			client_id: '5h4E1jWnJDWgPBnxwgR_m_R-2UxiRlNl7yARrbhHlMk', 
 			scope: 'read', 
@@ -7,7 +7,7 @@ async function oauth() {
 			response_type: 'code'
 		}
 	})
-	console.log(res)
+	console.log(res)*/
 	/*res = await axios.post('https://suzuri.jp/oauth/token', {
 		params: {
 			grant_type: 'authorization_code',
@@ -16,3 +16,16 @@ async function oauth() {
 	})*/
 }
 oauth()
+
+function getParam(name, url) {
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+let code = getParam('code')
+console.log(code)
